28e73ea0bb01764d8a58face6ac6de63609d1435
Alex <34627441+alex-yatseyko@users.noreply.github.com>
2024-10-02 19:16:47 -0400

    Book list GET and Book create POST API added

--- BookApp/api.py
+++ BookApp/api.py
@@ -1,15 +1,30 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import BookModel
 
+@api_view(['GET'])
 def BookListApi(request):
     # fetch from db
-    #books = BookModel.objects.all()
+    books = BookModel.objects.all()
 
-    books = [
-        {
-            "name": 'Test',
-            "author": "Alex"
-        }
-    ]
+    books = [{
+        'name': book.name,
+        "author": book.author,
+        "pages": book.pages
+    } for book in books]
 
     return Response(books)
+
+@api_view(['POST'])
+def BookCreateApi(request):
+    data = request.data
+
+    name = data.get('name')
+    author = data.get('author')
+    pages = data.get('pages')
+
+    BookModel(name=name, author=author, pages=pages).save()
+
+    return Response({
+        'message': 'Book is created'
+    })
--- BookApp/urls.py
+++ BookApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .api import BookListApi
+from .api import BookListApi, BookCreateApi
 
 urlpatterns = [
-    path('list', BookListApi)
+    path('list', BookListApi),
+    path('create', BookCreateApi),
 ]
